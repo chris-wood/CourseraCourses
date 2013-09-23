cf9f12589dce3ded977cfa916850589d0fa0ada2
caw <christopherwood07@gmail.com>
2013-09-22 18:00:18 -0700

    Removed redundant branch

--- assignments/ex1/recfun/src/main/scala/recfun/Main.scala
+++ assignments/ex1/recfun/src/main/scala/recfun/Main.scala
@@ -16,8 +16,8 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 && r == 0) 1
-    else if (c == r) pascal(c-1, r-1) // right side of triangle
-    else if (c == 0) pascal(c, r-1)   // left side of triangle
+    else if (c == r) pascal(c - 1, r - 1) // right side of triangle
+    else if (c == 0) pascal(c, r - 1) // left side of triangle
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
@@ -25,22 +25,21 @@ object Main {
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-		  def innerBalance(chars: List[Char], acc: Int) : Int = {
-		    if (chars.isEmpty) acc
-		    else if (acc < 0) -1 // short circuit, can't ever be < 0 or we have mismatch!
-		    else if (chars.head == '(') innerBalance(chars.tail, acc + 1)
-		    else if (chars.head == ')') innerBalance(chars.tail, acc - 1)
-		    else innerBalance(chars.tail, acc) // wasn't open or close, so just skip
-		  }
-    if (innerBalance(chars, 0) == 0) true
-    else false
+    def innerBalance(chars: List[Char], acc: Int): Int = {
+      if (chars.isEmpty) acc
+      else if (acc < 0) -1 // short circuit, can't ever be < 0 or we have mismatch!
+      else if (chars.head == '(') innerBalance(chars.tail, acc + 1)
+      else if (chars.head == ')') innerBalance(chars.tail, acc - 1)
+      else innerBalance(chars.tail, acc) // wasn't open or close, so just skip
+    }
+    innerBalance(chars, 0) == 0
   }
 
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    def innerCount(sum: Int, money:Int, coins: List[Int]) : Int = {
+    def innerCount(sum: Int, money: Int, coins: List[Int]): Int = {
       if (sum > money) 0 // didn't work
       else if (sum == money) 1 // worked
       else if (coins.isEmpty) 0 // no coins left to pick from, so it can't possibly work

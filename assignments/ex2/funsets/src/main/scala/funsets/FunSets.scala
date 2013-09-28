f6827580424fc13ade6dba0d53aa813c375659cc
Christopher Wood <christopherwood07@gmail.com>
2013-09-27 19:22:24 -0700

    Added the exercises

--- assignments/ex2/funsets/src/main/scala/funsets/FunSets.scala
+++ assignments/ex2/funsets/src/main/scala/funsets/FunSets.scala
@@ -18,32 +18,40 @@ object FunSets {
   def contains(s: Set, elem: Int): Boolean = s(elem)
 
   /**
-   * Returns the set of the one given element.
+   * Returns the set (a function) of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = (x : Int) => x == elem
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = {
+    (x : Int) => contains(s, x) || contains(t, x)
+  }
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = {
+    (x : Int) => contains(s, x) && contains(t, x)
+  }
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = {
+    (x : Int) => contains(s, x) && !contains(s, x)
+  }
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = {
+    (x : Int) => contains(s, x) && p(x) // only returns true if in the set and predicate holds
+  }
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.

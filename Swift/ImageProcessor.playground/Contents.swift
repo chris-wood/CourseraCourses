1cade1965a6b7a4459f653da0b479e1f77bd4834
Christopher Wood <christopherwood07@gmail.com>
2015-10-16 15:22:14 -0700

    coursera progress

--- Swift/ImageProcessor.playground/Contents.swift
+++ Swift/ImageProcessor.playground/Contents.swift
@@ -2,7 +2,32 @@
 
 import UIKit
 
-let image = UIImage(named: "sample")
+let image = UIImage(named: "sample")!
 
 
 // Process the image!
+let rgbaImage = RGBAImage(image: image)!;
+
+// Apply some basic filter
+for y in 0...rgbaImage.width {
+    for x in 0...rgbaImage.height {
+        let index = (x * rgbaImage.width) + y;
+        
+        var red = 0.0
+        var green = 0.0
+        var blue = 0.0
+        
+        // TODO: apply the filter here
+    }
+}
+
+class ImageProcessor {
+    
+    var _image:UIImage;
+    
+    init (image: UIImage) {
+        _image = image;
+    }
+    
+    
+}
